67db3f41c58343c98fb65b3b041f196ec805c481
Young Sun <young7869264s@163.com>
2023-06-13 09:57:49 +0800

    🐞 fix(embed modal): 增加两个模态的嵌入代码
    1. 增加对图像模态的嵌入；
    2. 增加对review summary模态的嵌入。

--- flan_as_encoder_for_amazon_item_imageANDself.py
+++ flan_as_encoder_for_amazon_item_imageANDself.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import numpy as np
 from transformers import pipeline
 import torch
 import time
@@ -10,9 +11,9 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class CustomPipeline:
-    def __init__(self, model_name):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, device_map={"": 'cuda:1'})
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, device_map={"": 'cuda:1'})
+    def __init__(self, model_name, gpu_index):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, device_map={"": 'cuda:'+str(gpu_index)})
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, device_map={"": 'cuda:'+str(gpu_index)})
 
     def get_embedding(self, input_text):
         input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
@@ -22,9 +23,6 @@ class CustomPipeline:
             hidden_states = outputs.encoder_last_hidden_state
         cls_embedding = hidden_states[:, 0, :]
         return cls_embedding
-
-model_name = "declare-lab/flan-alpaca-xl"
-custom_pipeline = CustomPipeline(model_name)
 
 
 def run(filename, write_filename_image, write_filename_description):
@@ -153,34 +151,131 @@ def embed_4_modal_meta(file_path, target_file_path):
             # 将time_left按照时分秒格式输出
             time_left = time.strftime("%H:%M:%S", time.gmtime(time_left))
             print('count:', count, 'poi_id:', poi_id, 'time_left:', time_left)
-
             count += 1
 
     target_file.flush()
     target_file.close()
 
-def embed_4_modal_imgdes():
-    return
+def embed_4_modal_image(file_path, target_file_path):
+    # 用于计算时间
+    count = 1
+    time_start = time.time()
+    # 先读取一遍filename记录总行数
+    total_num = 0
+    with open(file_path, 'r') as f:
+        for line in f:
+            total_num += 1
+
+    # 打开目标写入文件
+    target_file = open(target_file_path, 'w')
+
+    # 逐条读取filename
+    with open(file_path, 'r') as f:
+        for line in f:
+            # 解析json
+            line = json.loads(line)
+            kv = line.popitem()
+            poi_id = kv[0]
+            des_list = kv[1]
+
+            embeddings = []
+            for des in des_list: # 这里没有做空处理，因为image_des中没有空集合
+                cls_embedding = custom_pipeline.get_embedding(des)
+                cls_embedding = cls_embedding.tolist()
+                embeddings.append(cls_embedding)
+            # 求和成为一个embedding，形状跟单个cls_embedding一样
+            summed_embedding = np.sum(embeddings, axis=0)
+            # 归一化
+            normalized_embedding = summed_embedding / np.linalg.norm(summed_embedding)
+            normalized_embedding = normalized_embedding.tolist()
+            # 写入文件
+            target_file.write(json.dumps({poi_id: normalized_embedding}) + '\n')
+            # 计算剩余时间
+            time_end = time.time()
+            time_left = (time_end - time_start) / (count + 1) * (total_num - count)
+            # 将time_left按照时分秒格式输出
+            time_left = time.strftime("%H:%M:%S", time.gmtime(time_left))
+            print('count:', count, 'poi_id:', poi_id, 'time_left:', time_left)
+            count += 1
+            
+    target_file.flush()
+    target_file.close()
+
+def embed_4_modal_review_summary(file_path, target_file_path):
+    # 用于计算时间
+    count = 1
+    time_start = time.time()
+    # 先读取一遍filename记录总行数
+    total_num = 0
+    with open(file_path, 'r') as f:
+        for line in f:
+            total_num += 1
+
+    # 打开目标写入文件
+    target_file = open(target_file_path, 'w')
+
+    # 逐条读取filename
+    with open(file_path, 'r') as f:
+        for line in f:
+            # 解析json
+            line = json.loads(line)
+            kv = line.popitem() # [key, value]
+            poi_id = kv[0] # key
+            review_summary_list = kv[1] # value
+            
+            review_summary_embedding = None
+            # 判断list是否为空
+            if review_summary_list == []:
+                # list为空，处理与pois_des一致，对空字符串做嵌入
+                cls_embedding = custom_pipeline.get_embedding("")
+                review_summary_embedding = cls_embedding.tolist()
+            else:
+                embeddings = []
+                for review in review_summary_list:
+                    cls_embedding = custom_pipeline.get_embedding(review)
+                    cls_embedding = cls_embedding.tolist()
+                    embeddings.append(cls_embedding)
+                # 求和成为一个embedding，形状跟单个cls_embedding一样
+                summed_embedding = np.sum(embeddings, axis=0)
+                # 归一化
+                normalized_embedding = summed_embedding / np.linalg.norm(summed_embedding)
+                review_summary_embedding = normalized_embedding.tolist()
+            # 写入文件
+            target_file.write(json.dumps({poi_id: review_summary_embedding}) + '\n')
+            # 计算剩余时间
+            time_end = time.time()
+            time_left = (time_end - time_start) / (count + 1) * (total_num - count)
+            # 将time_left按照时分秒格式输出
+            time_left = time.strftime("%H:%M:%S", time.gmtime(time_left))
+            print('count:', count, 'poi_id:', poi_id, 'time_left:', time_left)
+            count += 1
+            
+    target_file.flush()
+    target_file.close()
 
 if __name__ == '__main__':
     # paremeters
     parser = argparse.ArgumentParser()
     parser.add_argument('--region', type=str, default='Hawaii', help='the region name of datasets(e.g. California)')
+    parser.add_argument('--gpu_index', type=str, default='3', help='the index of cuda')
     args, _ = parser.parse_known_args()
 
     parent_path = './dataset/' + args.region + '/'
+
+    model_name = "declare-lab/flan-alpaca-xl"
+    custom_pipeline = CustomPipeline(model_name, args.gpu_index)
     
-    # 处理poi_description，对其做嵌入
+    # 处理pois_description.json，对其做嵌入
     pois_description_file_path = parent_path + 'pois_description.json'
     modal_meta_embedding_file_path = parent_path + 'modal_meta_embedding.json'
     embed_4_modal_meta(pois_description_file_path, modal_meta_embedding_file_path)
 
-    # filename = 'amazon/meta_Musical_5core_with_imageDescription.json'
-    # write_filename_image = 'amazon/Amazon_Musical_image_embedding.json'
-    # write_filename_description = 'amazon/Amazon_Musical_description_embedding.json'
-    # run(filename, write_filename_image, write_filename_description)
+    # 处理image_description.json，对其做嵌入
+    image_description_file_path = parent_path + 'image_description.json'
+    modal_image_embedding_file_path = parent_path + 'modal_image_embedding.json'
+    embed_4_modal_image(image_description_file_path, modal_image_embedding_file_path)
 
-    # filename = 'amazon/meta_Video_Games_5core_with_imageDescription.json'
-    # write_filename_image = 'amazon/Amazon_Video_Games_image_embedding.json'
-    # write_filename_description = 'amazon/Amazon_Video_Games_description_embedding.json'
-    # run(filename, write_filename_image, write_filename_description)
+    # 处理review_summary.json，对其做嵌入
+    review_summary_file_path = parent_path + 'review_summary.json'
+    modal_review_summary_embedding_file_path = parent_path + 'modal_review_summary_embedding.json'
+    embed_4_modal_review_summary(review_summary_file_path, modal_review_summary_embedding_file_path)
